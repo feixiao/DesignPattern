9fa3c2c62548b8ea2494e751f50c2aac585f591d
feixiao <feixiao2020@sina.com>
2015-12-22 12:17:10 +0800

    SimpleFactory in C++

---
+++ C++/SimpleFactory/main.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+
+
+//  build with: g++  -Wall main.cpp -o simple_factory
+
+using namespace std;
+
+class Api
+{
+public:
+	virtual void Operation(string str) const = 0;
+	virtual ~Api(){cout << "Api::~Api()" << endl;};
+};
+
+class ImplA: public Api
+{
+public:
+	ImplA(){
+		cout << "ImplA::ImplA()" << endl;
+	}
+	~ImplA(){
+		cout << "ImplA::~ImplA()" << endl;
+	}
+
+	void Operation(string str) const {
+		cout << "ImplA::Operation():" + str  << endl;
+	}
+	
+};
+
+
+class ImplB: public Api
+{
+public:
+	ImplB(){
+		cout << "ImplB::ImplB()" << endl;
+	}
+	~ImplB(){
+		cout << "ImplB::ImplB()" << endl;
+	}
+
+	void Operation(string str) const {
+		cout << "ImplB::Operation():" + str  << endl;
+	}
+};
+
+class Factory{
+public:
+	static Api* CreateApi(int cond) {
+		return new ImplA();
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Api* api = Factory::CreateApi(1);
+	api->Operation("test");
+	delete api;
+	return 0;
+}
